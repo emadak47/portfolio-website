9b986a9c151da057c278588ac8b374338193e865
emadak47 <emadakhrasidleb@gmail.com>
2023-05-21 18:05:21 +0800

    standardise indentation -> 4 spaces

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,21 +4,21 @@ import '@/styles/globals.css';
 import { Montserrat } from 'next/font/google';
 
 const montserrate = Montserrat({
-  subsets: ['latin'],
-  variable: "--font-mont",
+    subsets: ['latin'],
+    variable: "--font-mont",
 });
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={`${montserrate.variable} font-mont bg-light w-full min-h-screen`}>
-        <NavBar />
-        <Component {...pageProps} />
-      </main>
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main className={`${montserrate.variable} font-mont bg-light w-full min-h-screen`}>
+                <NavBar />
+                <Component {...pageProps} />
+            </main>
+        </>
+    )
 }
