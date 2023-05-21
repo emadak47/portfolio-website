37c36b8a76654f876a87b3d1c2e3d27fbb90b50f
emadak47 <emadakhrasidleb@gmail.com>
2023-05-21 16:32:25 +0800

    configured fonts, colours, layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,23 @@
-import '@/styles/globals.css'
+import Head from 'next/head';
+import NavBar from '@/components/NavBar';
+import '@/styles/globals.css';
+import { Montserrat } from 'next/font/google';
+
+const montserrate = Montserrat({
+  subsets: ['latin'],
+  variable: "--font-mont",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${montserrate.variable} font-mont bg-light w-full min-h-screen`}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -11,6 +14,15 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        mont: ['var(--font-mont)', ...fontFamily.sans],
+      },
+      colors: {
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#B63E96", 
+        primaryDark: "#58E6D9", 
       },
     },
   },
